a893f526332b2516225918626083a91be5eaa27b
Eusebio Simango <eusebiosimango14@gmail.com>
2022-11-25 12:49:19 +0200

    calculate the fist five terms

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("HI")
+const sucessao: string = "2n-1"
+
+const calculateFiveFirstTerms = (sucessao: string) => {
+	[1, 2, 3, 4, 5].forEach(val => {
+		console.log(eval(sucessao.replace("n", `*${val}`)))
+	})
+}
+
+
+calculateFiveFirstTerms(sucessao)
