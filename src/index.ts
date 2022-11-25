ded7a059c50e4a20a9ca3ed68e840ee5aa8b32e5
Eusebio Simango <eusebiosimango14@gmail.com>
2022-11-25 13:09:46 +0200

    BugFixing: '.replace' to '.replaceAll' | @typescript - target from 'es2016' to 'es2021'

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ const sucessao: string = "2n-1n"
 
 const calculateFiveFirstTerms = (sucessao: string) => {
 	[1, 2, 3, 4, 5].forEach(val => {
-		console.log(eval(sucessao.replace("n", `*${val}`)))
+		console.log(eval(sucessao.replaceAll("n", `*${val}`)))
 	})
 }
 
 const calculateNFirstTerms = (sucessao: string, n: number) => {
 	for (let i = 1; i <= n; i++) {
-		console.log(eval(sucessao.replace("n", `*${i}`)))
+		console.log(eval(sucessao.replaceAll("n", `*${i}`)))
 	}
 }
 
